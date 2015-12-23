9cc5f892f0833218b509ff3ae5fd057883ca597a
Truong, Thanh <Thanh.Truong@Vencore.com>
2015-12-22 18:09:07 -0800

    add ng-module and test

---
+++ AngulaJumpstart.Tests/ng-test/MainCtrlSpec.js
@@ -0,0 +1,13 @@
+﻿describe("Controller: MainCtrl", function() {
+  beforeEach(module("MyApp"));
+  var MainCtrl, scope;
+  beforeEach(inject(function($controller) {
+    scope = {};
+    MainCtrl = $controller("MainCtrl", {
+      $scope: scope
+    });
+  }));
+  it("should have scope defined", function() {
+    expect(scope).toBeDefined();
+  });
+});
---
+++ AngulaJumpstart/Scripts/ng-scripts/MainCtrl.js
@@ -0,0 +1,7 @@
+﻿var myModule = angular.module('MyApp', []);
+
+myModule.controller('MainCtrl', ['$scope',
+  function ($scope) {
+    // I'm a lonely controller :( 
+  }
+]);
